b7810a0b65894605cf05503558a20494b2f4bae1
adnankuljancic <92022102+adnankuljancic@users.noreply.github.com>
2023-04-22 16:10:27 +0200

    basic folder structure with working route (#2)

---
+++ rest/routes/UserRoutes.php
@@ -0,0 +1,11 @@
+<?php
+Flight::route('GET /api/users', function ($id) {
+    echo 'Hi';
+});
+
+Flight::route('GET /api/users/@id', function ($id) {
+    Flight::json(Flight::userService()->getUserById($id));
+});
+
+
+?>
