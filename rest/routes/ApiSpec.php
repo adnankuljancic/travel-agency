050a3c581431023c8a11f030298639445afa1a24
Adnan Kuljancic <adnan.kuljancic@outlook.com>
2023-09-26 13:39:14 +0200

    Swagger part 1

---
+++ rest/routes/ApiSpec.php
@@ -0,0 +1,13 @@
+<?php
+
+
+/**
+ * @OA\Info(title="Journey", version="0.1", @OA\Contact(email="web", name="Web"))
+ * @OA\OpenApi(
+ *    @OA\Server(url="http://localhost/travel-agency/rest", description="Development Environment" ),
+ *    @OA\Server(url="https://journey.ba/rest", description="Production Environment" )
+ * ),
+ * @OA\SecurityScheme(securityScheme="ApiKeyAuth", type="apiKey", in="header", name="Authorization" )
+ */
+
+?>
